f27f5fcd38c38d36abe536f4049fc1538541af8b
Ronaldo Gueller <ronaldo.gueller@gmail.com>
2019-10-23 17:53:51 -0300

    Other Commit

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+// eslint-disable-next-line import/no-named-as-default
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.put('/users', AuthMiddleware, UserController.update);
 
 routes.post('/session', SessionController.store);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({
+        ok: true,
+    });
+});
 
 export default routes;
